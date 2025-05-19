5b85fc9dbd2be6bc62497916c5f19033d2347b83
Brody <113292266+b-geo@users.noreply.github.com>
2025-05-19 23:40:51 +0800

    gg

--- orchestration/dagster_pipeline/source_assets/snowflake/__init__.py
+++ orchestration/dagster_pipeline/source_assets/snowflake/__init__.py
@@ -4,7 +4,6 @@ from dagster import (
 )
 from . import snowflake_source_assets
 
-# adding type hints below to avoid pylint warnings
 snowflake_source_assets_list: List[SourceAsset] = [
     snowflake_source_assets.stg_laps,
     snowflake_source_assets.stg_telemetry
